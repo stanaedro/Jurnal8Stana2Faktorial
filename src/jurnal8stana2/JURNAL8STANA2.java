75727cfbacd9ac3e5741cade460e37a18891558e
STANA_6706150028 <stanaedroswargaraa@gmail.com>
2017-04-06 09:37:29 +0700

    Add files via upload

---
+++ src/jurnal8stana2/JURNAL8STANA2.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jurnal8stana2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Praktikan
+ */
+public class JURNAL8STANA2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scn = new Scanner(System.in);
+        System.out.print("input tinggi : ");
+        int n=scn.nextInt();
+//erickstdy.blogspot.com      
+
+    for (int i = 1; i < n; i += 2) {
+      for (int j = 0; j < 4 - i / 2; j++)
+        System.out.print(" ");
+
+      for (int j = 0; j < i; j++)
+        System.out.print("*");
+        System.out.print("\n");
+    }
+
+    for (int i = n; i > 0; i -= 2) {
+      for (int j = 0; j < 4 - i / 2; j++)
+        System.out.print(" ");
+
+      for (int j = 0; j < i; j++)
+        System.out.print("*");
+        System.out.print("\n");
+    }
+  }
+    }
+    
